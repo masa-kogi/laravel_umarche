73878e9f7808750ccada5069bf6a6b9768781094
masa-kogi <mk54325@yahoo.co.jp>
2022-08-05 00:18:15 +0900

    multiLogin

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,16 @@ use App\Http\Controllers\lifeCycleTestController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return view('user.dashboard');
+})->middleware(['auth:users'])->name('dashboard');
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 Route::get('/servicecontainertest', [lifeCycleTestController::class, 'showServiceContainerTest']);
 Route::get('/serviceprovidertest', [lifeCycleTestController::class, 'showServiceProviderTest']);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
