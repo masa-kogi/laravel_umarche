0946268b55ae266e4530079d9989bc277e3c86f3
masa-kogi <mk54325@yahoo.co.jp>
2022-08-24 00:43:24 +0900

    image, create, validation

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UploadImageRequest;
+
 
 class ImageController extends Controller
 {
@@ -35,8 +37,8 @@ class ImageController extends Controller
     public function index()
     {
         $images = Image::where('owner_id', Auth::id())
-        ->orderBy('updated_at', 'desc')
-        ->get();
+            ->orderBy('updated_at', 'desc')
+            ->paginate(20);
 
         return view('owner.images.index', compact('images'));
     }
@@ -48,7 +50,7 @@ class ImageController extends Controller
      */
     public function create()
     {
-        //
+        return view('owner.images.create');
     }
 
     /**
@@ -57,9 +59,9 @@ class ImageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UploadImageRequest $request)
     {
-        //
+        dd($request);
     }
 
     /**
