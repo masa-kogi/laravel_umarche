8f566d504b3487128c6c05eae903044ca30f1ec6
masa-kogi <mk54325@yahoo.co.jp>
2022-08-25 01:41:38 +0900

    category

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -19,26 +19,32 @@ class ImageSeeder extends Seeder
             [
                 'owner_id' => 1,
                 'filename' => 'sample1.jpg',
+                'title' => null
             ],
             [
                 'owner_id' => 1,
                 'filename' => 'sample2.jpg',
+                'title' => null
             ],
             [
                 'owner_id' => 1,
                 'filename' => 'sample3.jpg',
+                'title' => null
             ],
             [
                 'owner_id' => 1,
                 'filename' => 'sample4.jpg',
+                'title' => null
             ],
             [
                 'owner_id' => 1,
                 'filename' => 'sample5.jpg',
+                'title' => null
             ],
             [
                 'owner_id' => 1,
                 'filename' => 'sample6.jpg',
+                'title' => null
             ],
         ]);
     }
