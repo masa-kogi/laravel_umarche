978230ee5096a0a5cb4646507419a96a8734a2ba
masa-kogi <mk54325@yahoo.co.jp>
2022-08-27 13:52:27 +0900

    product index, eager loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -4,9 +4,31 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
+use App\Models\Image;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('product');
+            if (!is_null($id)) {
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int) $productsOwnerId;
+                if ($productId !== Auth::id()) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +36,13 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        // $products = Owner::findOrFail(Auth::id())->shop->product;
+        $ownerInfo = Owner::with('shop.product.imageFirst')->where('id', Auth::id())->get();
+        // dd($ownerInfo);
+        // foreach($ownerInfo as $owner) {
+        //     dd($owner->shop->product);
+        // }
+        return view('owner.products.index', compact('ownerInfo'));
     }
 
     /**
@@ -24,7 +52,6 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
