fc3e4a613512e00d0879233aaed63027563b73c9
masa-kogi <mk54325@yahoo.co.jp>
2022-08-28 15:39:04 +0900

    product create

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -3,10 +3,11 @@ if($name === 'image1') { $modal = 'modal-1'; }
 if($name === 'image2') { $modal = 'modal-2'; }
 if($name === 'image3') { $modal = 'modal-3'; }
 if($name === 'image4') { $modal = 'modal-4'; }
+if($name === 'image5') { $modal = 'modal-5'; }
 @endphp
 
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
         <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
             <header class="modal__header">
                 <h2 class="text-xl text-gray-700" id="{{ $modal }}-title">
@@ -18,10 +19,10 @@ if($name === 'image4') { $modal = 'modal-4'; }
                 <div class="flex flex-wrap">
                     @foreach ($images as $image)
                     <div class="w-1/4 p-2 md:p-4">
-                            <div class="border rounded-md p-2 md:p-4">
-                                <img class="image" data-id="{{ $name }}_{{ $image->id }}" data-file="{{ $image->filename }}" data-path="{{ asset('storage/products/') }}" data-modal="{{ $modal }}" src="{{ asset('storage/products/' . $image->filename) }}" alt="">
-                                <div class="text-gray-700">{{ $image->title }}</div>
-                            </div>
+                        <div class="border rounded-md p-2 md:p-4">
+                            <img class="image" data-id="{{ $name }}_{{ $image->id }}" data-file="{{ $image->filename }}" data-path="{{ asset('storage/products/') }}" data-modal="{{ $modal }}" src="{{ asset('storage/products/' . $image->filename) }}" alt="">
+                            <div class="text-gray-700">{{ $image->title }}</div>
+                        </div>
                     </div>
                     @endforeach
                 </div>
@@ -34,7 +35,7 @@ if($name === 'image4') { $modal = 'modal-4'; }
 </div>
 
 <div class="flex justify-around items-center mb-4">
-    <a data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイル選択</a>
+    <a class="py-2 px-4 bg-gray-200" data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイル選択</a>
     <div class="w-1/4">
         <img id="{{ $name }}_thumbnail" src="" alt="">
     </div>
