0ea136e74fd0f55db203c475897ac06b7017948f
masa-kogi <mk54325@yahoo.co.jp>
2022-08-30 00:33:44 +0900

    delete image used for a product

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use App\Models\Image;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -64,8 +65,8 @@ class ImageController extends Controller
     public function store(UploadImageRequest $request)
     {
         $imageFiles = $request->file('files');
-        if(!is_null($imageFiles)) {
-            foreach($imageFiles as $imageFile) {
+        if (!is_null($imageFiles)) {
+            foreach ($imageFiles as $imageFile) {
                 $fileNameToStore = ImageService::upload($imageFile, 'products');
                 Image::create([
                     'owner_id' => Auth::id(),
@@ -119,7 +120,6 @@ class ImageController extends Controller
                 'message' => '画像を更新しました',
                 'status' => 'info'
             ]);
-
     }
 
     /**
@@ -131,9 +131,37 @@ class ImageController extends Controller
     public function destroy($id)
     {
         $image = Image::findOrFail($id);
+
+        $imageInProducts = Product::where('image1', $image->id)
+            ->orWhere('image2', $image->id)
+            ->orWhere('image3', $image->id)
+            ->orWhere('image4', $image->id)
+            ->get();
+
+        if ($imageInProducts) {
+            $imageInProducts->each(function ($product) use ($image) {
+                if ($product->image1 === $image->id) {
+                    $product->image1 = null;
+                    $product->save();
+                }
+                if ($product->image2 === $image->id) {
+                    $product->image2 = null;
+                    $product->save();
+                }
+                if ($product->image3 === $image->id) {
+                    $product->image3 = null;
+                    $product->save();
+                }
+                if ($product->image4 === $image->id) {
+                    $product->image4 = null;
+                    $product->save();
+                }
+            });
+        }
+
         $filePath = 'public/products/' . $image->filename;
 
-        if(Storage::exists($filePath)) {
+        if (Storage::exists($filePath)) {
             Storage::delete($filePath);
         }
 
