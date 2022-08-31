9b5a65ff970ad13d1dfc84b3fa5a9d5c14a5fa87
masa-kogi <mk54325@yahoo.co.jp>
2022-08-31 23:28:36 +0900

    stock information of product detail

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
@@ -14,7 +15,7 @@ class ItemController extends Controller
     {
         $this->middleware('auth:users');
     }
-    
+
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -46,7 +47,13 @@ class ItemController extends Controller
     public function show($id)
     {
         $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+            ->sum('quantity');
 
-        return view('user.show', compact('product'));
+        if($quantity > 9) {
+            $quantity = 9;
+        }
+
+        return view('user.show', compact('product', 'quantity'));
     }
 }
